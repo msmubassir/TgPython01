bb69e639dbc0e7f968f8b50b6596adb756938ddd
msmubassir <125449220+msmubassir@users.noreply.github.com>
2024-06-26 15:56:35 +0600

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def send():
     return 'Message sent!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
